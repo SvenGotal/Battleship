e5c82ed4934eabc99718d827379be11f580efe47
Ivan Bitunjac <ivbitunj@vsite.hr>
2020-03-18 21:38:52 +0100

    Create model and unit tests Create Square

---
+++ Model/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.BattleShip.Model
+{
+    public class Square
+    {
+        public Square(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+        public readonly int row;
+        public readonly int column;
+    }
+}
