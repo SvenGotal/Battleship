a560b39efa4e5c911425eb7ff1711ad0df83b058
Ivan Zoric <ivzoric@vsite.hr>
2020-03-18 21:39:04 +0100

    Create Model, UnitTests and square

---
+++ Model/square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class square
+    {
+        public square(int r, int c)
+        {
+            row = r;
+            column = c;
+        }
+        public readonly int row;
+        public readonly int column;
+    }
+}
---
+++ UnitTests/TestSquare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestSquare
+    {
+        [TestMethod]
+        public void squareConstructorCreatesSquareWithGivenPosition()
+        {
+            square S = new square(1, 8);
+            Assert.AreEqual(1,S.row);
+            Assert.AreEqual(8, S.column);
+        }
+    }
+}
