62ac68da04fa03f8e0685c4b42629171a2079755
Bruno Čale <50056973+brcale@users.noreply.github.com>
2020-03-18 21:51:10 +0100

    Model and unit tests created

---
+++ UnitTests/TestSquares.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestSquares
+    {
+        [TestMethod]
+        public void SquareConstructorCreateSquareWithGivenPosition()
+        {
+            Square s = new Square(1, 8);
+            Assert.AreEqual(1, s.Row);
+            Assert.AreEqual(8, s.Col);
+        }
+    }
+}
+
