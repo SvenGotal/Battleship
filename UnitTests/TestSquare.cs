b0642cd9f969ed474a3d5ab6c1c48576164e5952
Marko Vitković <mavitkov@vsite.hr>
2020-03-25 13:45:22 +0100

    rename UnitTest file

--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Vsite.Oom.Battleship.Model.UnitTest
+namespace Vsite.Oom.Battleship.Model.UnitTests
 {
 	[TestClass]
 	public class TestSquare
 	{
 		[TestMethod]
-		public void SquareConstructorCreatesSquareWithGivenPosition()
+		public void SquareConstructorCreateSquareWithGivenPosition()
 		{
 			Square s = new Square(1, 8);
 			Assert.AreEqual(1, s.Row);
