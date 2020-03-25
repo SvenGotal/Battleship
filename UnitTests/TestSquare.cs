4d88381579aa48f1bf635f08688854bd26327170
Ivan Zoric <ivzoric@vsite.hr>
2020-03-26 00:02:45 +0100

    Fix compilation error

--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Vsite.Oom.Battleship.Model.UnitTests
+namespace Vsite.Oom.Battleship.Model
 {
     [TestClass]
     public class TestSquare
