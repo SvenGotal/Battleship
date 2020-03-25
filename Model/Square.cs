3843610e1382020ea5956ed448e680b5d101d1e6
Senad Mandžić <semandzi@vsite.hr>
2020-03-25 22:59:47 +0100

    Create Model and Unit tests Create Square

---
+++ Model/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.oom.Battleship.Model
+{
+    public class Square
+    {
+        public Square(int rows,int columns)
+        {
+
+            Rows = rows;
+            Columns = columns;
+        }
+
+        public readonly int Rows;
+        public readonly int Columns;
+    }
+}
---
+++ UnitTests/UnitTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestSquare
+    {
+        [TestMethod]
+        public void SquareConstructorsCreateSquareWithGivenPosition()
+        {
+            Square s = new Square(1, 8);
+            Assert.AreEqual(1, s.Rows);
+            Assert.AreEqual(8, s.Columns);
+        }
+    }
+}
