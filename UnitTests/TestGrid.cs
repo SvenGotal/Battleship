8775301c07c5902f218cb1116c5e64819d2f58ab
ivek81cro <ivek81cro@gmail.com>
2020-03-25 21:34:38 +0100

    Add EliminateSquares method and test for it

--- UnitTests/TestGrid.cs
+++ UnitTests/TestGrid.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -27,6 +28,27 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
 
             foreach (var sequence in result)
                 Assert.AreEqual(3, sequence.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementForShipReturns3PlacementsForShipOfLenth2InHorizontalGrid1x6AfterSquareIsEliminated()
+        {
+            Grid g = new Grid(1, 6);
+            g.EliminateSquares(new List<Square> { new Square(0, 2) });
+            var result = g.GetAvailablePlacements(2);
+            Assert.AreEqual(3, result.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementForShipReturns2PlacementsForShipOfLenth2InVerticallGrid5x1AfterSquareIsEliminated()
+        {
+            Grid g = new Grid(5, 1);
+            g.EliminateSquares(new List<Square> { new Square(1, 0) });
+            var result = g.GetAvailablePlacements(2);
+            Assert.AreEqual(2, result.Count());
+
+            foreach (var sequence in result)
+                Assert.AreEqual(2, sequence.Count());
         }
     }
 }
