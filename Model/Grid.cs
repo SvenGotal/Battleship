d57d7f7f46f65f13d5115a71f3f6dae47122e59f
Fran Safranec <Fran@DESKTOP-QN7J9VS>
2020-03-25 21:13:21 +0100

    create Grid and tests for it

---
+++ Model/Grid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Grid
+    {
+        public Grid(int rows,int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+        }
+
+        public readonly int Rows;
+        public readonly int Columns;
+
+        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
