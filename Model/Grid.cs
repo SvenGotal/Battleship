49a29f062f8cc4c8dd014804e0aa4a4d2c64f4fa
Josip Vučković <39704449+JosipVuckovic@users.noreply.github.com>
2020-03-25 21:12:22 +0100

    Create Grid and tests for it

---
+++ Model/Grid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Grid
+    {
+        public readonly int Rows;
+        public readonly int Columns;
+        public Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+        }
+
+        public IEnumerable<IEnumerable<Square>> GetAviablePlacments(int length)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
---
+++ UnitTests/TestGrid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void GetAviablePlacmentsForShipReturns2PlacmentsForShipOfLength3InHorizontalGrid1x4()
+        {
+            Grid g = new Grid(1,4);
+            var result = g.GetAviablePlacments(3);
+            Assert.AreEqual(2, result.Count());
+
+            Assert.AreEqual(3, result.First().Count());
+            Assert.AreEqual(3, result.Last().Count());
+
+        }
+
+        [TestMethod]
+        public void GetAviablePlacmentsForShipReturns3PlacmentsForShipOfLength3InVerticalGrid5x1()
+        {
+            Grid g = new Grid(5, 1);
+            var result = g.GetAviablePlacments(3);
+            Assert.AreEqual(3, result.Count());
+
+            foreach (var secquence in result)
+            {
+                Assert.AreEqual(3, secquence.Count());
+            }
+        }
+    }
+}
