94359f4255bf09c9d8e40a982a4cddb6454b99b6
Domagoj Salkić <dosalkic@vsite.hr>
2020-04-01 20:46:35 +0200

    Get available horizontal placements

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Vsite.Oom.Battleship.Model
 {
+    using Placement = IEnumerable<Square>;
     public class Grid
     {
         public Grid(int rows, int columns)
@@ -22,15 +24,46 @@ namespace Vsite.Oom.Battleship.Model
             }
         }
 
-        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        public IEnumerable<Placement> GetAvailablePlacements(int length)
         {
-            throw new NotImplementedException();
+            return GetAvailableHorizontalPlacements(length).Concat(GetAvailableVerticalPlacements(length));
         }
 
-        public void EliminateSquares(IEnumerable<Square> squares)
+        public void EliminateSquares(Placement toEliminate)
         {
-            throw new NotImplementedException();
+            foreach (var square in toEliminate)
+            {
+                squares[square.Row, square.Column] = null;
+            }
         }
+
+        private IEnumerable<Placement> GetAvailableHorizontalPlacements(int length)
+        {
+            var result = new List<List<Square>>();
+            for (int r = 0; r< Rows; ++r)
+            {
+                int counter = 0;
+                for (int c = 0; c < Columns; ++c)
+                {
+                    if (squares[r, c] != null)
+                        ++counter;
+                    else
+                        counter = 0;
+                    if (counter >= length)
+                    {
+                        List<Square> seq = new List<Square>();
+                        for (int first = c - length + 1; first <= c; ++first)
+                            seq.Add(squares[r,first]);
+                        result.Add(seq);                        
+                    }
+                }
+            }
+
+            return result;
+        }
+
+      //  private IEnumerable<Placement> GetAvailableVerticalPlacements(int length)
+
 
         public readonly int Rows;
         public readonly int Columns;
