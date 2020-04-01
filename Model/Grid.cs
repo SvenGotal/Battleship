99184f06b90ec14c2ac0e5b938f67427dacea0f5
Ante Mišković <amiskovic0101@gmail.com>
2020-04-01 20:46:17 +0200

    Implement GetAvailableHorizontalPlacements method.

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
+    using Placement = IEnumerable<Square>;
     public class Grid
     {
         public Grid(int rows, int columns)
@@ -20,16 +21,45 @@ namespace Vsite.Oom.Battleship.Model
                     squares[r, c] = new Square(r, c);               
             }
         }
-        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        public IEnumerable<Placement> GetAvailablePlacements(int length)
+        {
+            return GetAvailableHorizontalPlacements(length).Concat(GetAvailableVerticalPlacements(length));
+        }
+        public void EliminateSquares(Placement toEliminate)
+        {
+            foreach(var square in toEliminate)
+            {
+                squares[square.Row, square.Column] = null;
+            }
+        }
+        private IEnumerable<Placement> GetAvailableHorizontalPlacements(int length)
+        {
+            var result = new List<List<Square>>();
+            for(int r = 0; r < Rows; ++r)
+            {
+                int counter = 0;
+                for(int c = 0; c < Columns; ++c)
+                {
+                    if (squares[r, c] != null)
+                        ++counter;
+                    else
+                        counter = 0;
+                    if (counter >= length)
+                    {
+                        List<Square> seq = new List<Square>();
+                        for (int first = c - length + 1; first <= c; ++first)
+                            seq.Add(squares[r, first]);
+                        result.Add(seq);
+                    }
+                }
+            }
+            return result;
+        }
+        private IEnumerable<Placement> GetAvailableVerticalPlacements(int length)
         {
             throw new NotImplementedException();
         }
-        public void EliminateSquares(IEnumerable<Square> squares)
-        {
-            throw new NotImplementedException();
-        }
-        
-              
+
         public readonly int Rows;
         public readonly int Columns;
         private Square[,] squares;
