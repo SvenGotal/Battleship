da58df23ad3d058dbdfe5b71541b365f1264bfd2
Marko Vitković <mavitkov@vsite.hr>
2020-04-01 21:28:55 +0200

    fix errors

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -15,24 +15,25 @@ namespace Vsite.Oom.Battleship.Model
 			squares = new Square[Rows, Cols];
 			for (int r = 0; r < Rows; ++r)
 			{
-				for (int c = 0; r < Cols; ++c)
+				for (int c = 0; c < Cols; ++c)
 					squares[r, c] = new Square(r, c);
 			}
 		}
 		public IEnumerable<Placement> GetAvailablePlacments(int length)
 		{
 			if(length != 1)
-			{
+				return GetAvailableHorizontalPlacements(length).Concat(GetAvailableVerticalPlacements(length));
+			
 				List<List<Square>> result = new List<List<Square>>();
 				for (int r = 0; r < Rows; ++r)
 				{
-					for (int c = 0; r < Cols; ++c)
+					for (int c = 0; c < Cols; ++c)
 						if (squares[r, c] != null)
 							result.Add(new List<Square> { squares[r, c] });
 				}
 				return result;
-			}
-			return GetAvailableHorizontalPlacements(length).Concat(GetAvailableVerticalPlacements(length));
+			
+			
 		}
 		public void EliminateSquares(Placement toEliminate)
 		{
