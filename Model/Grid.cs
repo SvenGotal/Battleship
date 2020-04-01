b69bfa634e583442640ea537299dd5ca9b7b62ee
Ivan Mihin <ivmihin@vsite.hr>
2020-04-01 21:41:00 +0200

    Create available placement methods and limited queue

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
+    using Placement = IEnumerable<Square>;
     public class Grid
     {
         public Grid(int rows, int columns)
@@ -21,17 +22,76 @@ namespace Vsite.Oom.Battleship.Model
                 }
             }
         }
-
-        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        public IEnumerable<Placement> GetAvailablePlacements(int Length)
         {
-            throw new NotImplementedException();
-        }
 
-        public void EliminateSquares(IEnumerable<Square> squares)
+            return GetAvailableHorizontalPlacements(Length).Concat(GetAvailableVerticalPlacements(Length));
+        }
+        public void EliminateSquares(Placement toEliminate)
         {
-            throw new NotImplementedException();
+            foreach (var square in toEliminate)
+            {
+                squares[square.Row, square.Column] = null;
+            }
         }
-
+        private IEnumerable<Placement> GetAvailableHorizontalPlacements(int Length)
+        {
+            var result = new List<List<Square>>();
+            for (int r = 0; r < Rows; ++r)
+            {
+                int counter = 0;
+                for (int c = 0; c < Columns; ++c)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                    if (counter >= Length)
+                    {
+                        List<Square> seq = new List<Square>();
+                        for (int first = c - Length + 1; first <= c; ++first)
+                        {
+                            seq.Add(squares[r, first]);
+                        }
+                        result.Add(seq);
+                    }
+                }
+            }
+            return result;
+        }
+        private IEnumerable<Placement> GetAvailableVerticalPlacements(int Length)
+        {
+            var result = new List<List<Square>>();
+            for (int c = 0; c < Columns; ++c)
+            {
+                int counter = 0;
+                for (int r = 0; r < Rows; ++r)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                    if (counter >= Length)
+                    {
+                        List<Square> seq = new List<Square>();
+                        for (int first = r - Length + 1; first <= r; ++first)
+                        {
+                            seq.Add(squares[first, c]);
+                        }
+                        result.Add(seq);
+                    }
+                }
+            }
+            return result;
+        }
         public readonly int Rows;
         public readonly int Columns;
 
--- UnitTests/TestGrid.cs
+++ UnitTests/TestGrid.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
 {
@@ -18,29 +18,29 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             Assert.AreEqual(3, result.First().Count());
             Assert.AreEqual(3, result.Last().Count());
         }
-
         [TestMethod]
-        public void GetAvailablePlacementsForShipReturns2PlacementsForShipOfLength3InVerticalGrid5x1()
+        public void GetAvailablePlacementsForShipReturns3PlacementsForShipOfLength3InVerticalGrid5x1()
         {
             Grid g = new Grid(5, 1);
             var result = g.GetAvailablePlacements(3);
             Assert.AreEqual(3, result.Count());
-
             foreach (var sequence in result)
             {
                 Assert.AreEqual(3, sequence.Count());
             }
         }
-
         [TestMethod]
         public void GetAvailablePlacementsForShipReturns3PlacementsForShipOfLength2InHorizontalGrid1x6AfterSquareIsEliminated()
         {
-            Grid g = new Grid(1, 4);
+            Grid g = new Grid(1, 6);
             g.EliminateSquares(new List<Square> { new Square(0, 2) });
             var result = g.GetAvailablePlacements(2);
             Assert.AreEqual(3, result.Count());
+            foreach (var sequence in result)
+            {
+                Assert.AreEqual(2, sequence.Count());
+            }
         }
-
         [TestMethod]
         public void GetAvailablePlacementsForShipReturns2PlacementsForShipOfLength2InVerticalGrid5x1AfterSquareIsEliminated()
         {
@@ -48,7 +48,6 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             g.EliminateSquares(new List<Square> { new Square(1, 0) });
             var result = g.GetAvailablePlacements(2);
             Assert.AreEqual(2, result.Count());
-
             foreach (var sequence in result)
             {
                 Assert.AreEqual(2, sequence.Count());
