8eff720e8ae7e5477b85108cfc0549365882393a
Senad Mandžić <semandzi@vsite.hr>
2020-04-02 01:01:00 +0200

    ImplementGetAvailableHorizontalPlaces

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vsite.oom.Battleship.Model
 {
+    using Placement = IEnumerable<Square>;
     public class Grid
     {
         public Grid(int rows,int columns)
@@ -20,15 +21,55 @@ namespace Vsite.oom.Battleship.Model
             }
 
         }
-        public IEnumerable<IEnumerable<Square>>GetAvailablePlacments(int length)
+        public IEnumerable<Placement> GetAvailablePlacements(int length)
+        {
+            return GetAvaillableHorizontalPlacements(length).Concat(GetAvailableVerticalPlacements(length)); ;
+        }
+
+        public void EliminateSquares(Placement  eliminate)
+        {
+
+            foreach(var square in eliminate )
+            {
+                squares[square.Rows, square.Columns] = null;
+            }
+
+
+        }
+
+        private IEnumerable<Placement> GetAvaillableHorizontalPlacements(int length)
+        {
+            var result = new List<List<Square>>();
+            for (int r = 0; r < Rows; ++r)
+            {
+                int counter = 0;
+                for (int c = 0; c < Columns; ++c)
+                {
+                    if (squares[r, c] != null)
+                        ++counter;
+                    else
+                        counter = 0;
+                    if(counter>=length)
+                    {
+                        List<Square> seq = new List<Square>();
+                        for (int first = c - length + 1; first <= c; ++first)
+                        {
+                            seq.Add(squares[r, first]);
+                        }
+
+                        result.Add(seq);
+
+                    }
+                }
+            }
+            return result;
+        }
+        private IEnumerable<Placement> GetAvailableVerticalPlacements(int height)
         {
             throw new NotImplementedException();
         }
 
-        public void EliminateSquares(IEnumerable<Square> squares)
-        {
-            throw new NotImplementedException();
-        }
+
         public readonly int Rows;
         public readonly  int Columns;
         private Square[,] squares;
--- UnitTests/TestGrid.cs
+++ UnitTests/TestGrid.cs
@@ -11,7 +11,7 @@ namespace Vsite.oom.Battleship.Model.UnitTests
         public void GetAvaillablePlacmentForShipReturns2PlacmentsForShipOfLength3InHorizontalGrid1x4()
         {
             Grid g = new Grid(1, 4);
-            var result = g.GetAvailablePlacments(3);
+            var result = g.GetAvailablePlacements(3);
             Assert.AreEqual(2, result.Count());
             Assert.AreEqual(3, result.First().Count());
             Assert.AreEqual(3, result.Last().Count());
@@ -21,11 +21,11 @@ namespace Vsite.oom.Battleship.Model.UnitTests
         public void GetAvaillablePlacmentForShipReturns3PlacmentsForShipOfLength3InVerticalGrid5x1()
         {
             Grid g = new Grid(5, 1);
-            var result = g.GetAvailablePlacments(3);
+            var result = g.GetAvailablePlacements(3);
             Assert.AreEqual(3, result.Count());
             foreach (var sequence in result)
 
-                Assert.AreEqual(3, result.Last().Count());
+                Assert.AreEqual(3, sequence.Count());
         }
 
 
@@ -33,17 +33,17 @@ namespace Vsite.oom.Battleship.Model.UnitTests
         {
             Grid g = new Grid(1, 6);
             g.EliminateSquares(new List<Square>{new Square(0, 2)});
-            var result = g.GetAvailablePlacments(2);
+            var result = g.GetAvailablePlacements(2);
             Assert.AreEqual(3, result.Count());
         }
 
         [TestMethod]
-        public void GetAvaillablePlacmentForShipReturns2PlacmentsForShipOfLength2InVerticalGrid5x1AfterSquareIsEliminated ()
+        public void GetAvaillablePlacmentForShipReturns2PlacmentsForShipOfLength2InVerticalGrid5x1AfterSquareIsEliminated()
         {
             Grid g = new Grid(5, 1);
-            g.EliminateSquares(new List<Square> { new Square(1, 0) });
+            g.EliminateSquares(new List<Square>{new Square(1, 0)});
 
-            var result = g.GetAvailablePlacments(2);
+            var result = g.GetAvailablePlacements(2);
             Assert.AreEqual(2, result.Count());
             foreach (var sequence in result)
 
