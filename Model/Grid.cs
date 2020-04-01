ded8843f36d6fcb890e40ae2f701719b5b9c4f84
Roland Rostohar <rorostoh@vsite.hr>
2020-04-01 20:46:12 +0200

    Implement GetAvailableHorizontalPlacements method

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
+	using Placement = IEnumerable<Square>;
 	public class Grid
 	{
 
@@ -27,12 +28,51 @@ namespace Vsite.Oom.Battleship.Model
 			}
 		}
 
-		public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int lenght)
+		public IEnumerable<Placement> GetAvailablePlacements(int lenght)
 		{
-			throw new NotImplementedException();
+			return GetAwailableHorizontalPlacements(lenght).Concat(GetAwailableVerticalPlacements(lenght));
 		}
 
-		public void EliminateSquares(IEnumerable<Square> squares)
+		public void EliminateSquares(Placement toEliminate)
+		{
+			foreach (var square in toEliminate)
+			{
+				squares[square.Row, square.Column] = null;
+			}
+		}
+
+		private IEnumerable<Placement> GetAwailableHorizontalPlacements(int lenght)
+		{
+			var result = new List<List<Square>>();
+
+			for (int r = 0; r < Rows; r++)
+			{
+				int counter = 0;
+
+				for (int c = 0; c < Columns; c++)
+				{
+					if (squares[r, c] != null)
+						++counter;
+					else
+						counter = 0;
+
+					if (counter >= lenght)
+					{
+						List<Square> seq = new List<Square>();
+						for (int first = c - lenght + 1; first <= c; first++)
+						{
+							seq.Add(squares[r, first]);
+						}
+
+						result.Add(seq);
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private IEnumerable<Placement> GetAwailableVerticalPlacements(int lenght)
 		{
 			throw new NotImplementedException();
 		}
