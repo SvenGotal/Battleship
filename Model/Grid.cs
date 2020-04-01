fabbbc40b438eb7fba55facab696b0f0e1fd20eb
Sara Ćuzele Papata <saracuzele@gmail.com>
2020-04-01 20:47:09 +0200

    Implement GetAvailableHorizontal and Vertical placements

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
+    using Placement = IEnumerable<Square>;
     public class Grid
     {
+
         public Grid(int rows, int columns)
         {
             Rows = rows;
@@ -25,14 +27,51 @@ namespace Vsite.Oom.Battleship.Model
         public readonly int Rows;
         public readonly int Columns;
 
-        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        public IEnumerable<Placement> GetAvailablePlacements(int length)
+        {
+            return GeAvailableHorizontalPlacements(length).Concat(GetAvailableVerticalPlacements(length));
+        }
+
+        private IEnumerable<Placement> GeAvailableHorizontalPlacements(int length)
+        {
+            var result = new List<List<Square>>();
+            for (int r = 0; r < Rows; r++)
+            {
+                int counter = 0;
+                for (int c = 0; c < Columns; c++)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                    if (counter >= length)
+                    {
+                        var seq = new List<Square>();
+                        for (int i = c - length + 1; i <= c; i++)
+                        {
+                            seq.Add(squares[r, i]);
+                        }
+                        result.Add(seq);
+                    }
+                }
+            }
+            return result;
+        }
+        private IEnumerable<Placement> GetAvailableVerticalPlacements(int length)
         {
             throw new NotImplementedException();
         }
 
-        public void EliminateSqure(IEnumerable<Square> squares)
+        public void EliminateSqure(Placement toEliminate)
         {
-
+            foreach (var item in toEliminate)
+            {
+                squares[item.Row, item.Column] = null;
+            }
         }
 
         private Square[,] squares;
