480a467f122c903691c05ee32eeda77942f75c51
Fran Safranec <Fran@DESKTOP-QN7J9VS>
2020-04-08 20:58:33 +0200

    add shipwright

---
+++ Model/ShipWright.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class ShipWright
+    {
+
+        public ShipWright(int rows,int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+ 
+        }
+
+        public Fleet CreateFleet(IEnumerable<int> shipLengths)
+        {
+
+            List<int> lengths = new List<int>(shipLengths);
+
+            //grid = new Grid(rows, columns);
+
+            Fleet fleet = new Fleet();
+
+            
+
+
+
+
+            throw new NotImplementedException();
+        }
+
+      
+
+        private readonly int rows;
+            private readonly int columns;
+
+
+    }
+}
--- Model/fleet.cs
+++ Model/fleet.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Vsite.Oom.Battleship.Model
 {
-    public class fleet
+    public class Fleet
     {
 
 
