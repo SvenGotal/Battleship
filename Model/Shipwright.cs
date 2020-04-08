8272698fe048cf1ffea3db6ccd67b32c801925cb
Josip Vučković <39704449+JosipVuckovic@users.noreply.github.com>
2020-04-08 20:58:27 +0200

    Add Shipwright and CreateFleet

--- Model/Shipwright.cs
+++ Model/Shipwright.cs
@@ -8,5 +8,47 @@ namespace Vsite.Oom.Battleship.Model
 {
     public class Shipwright
     {
+
+        private readonly int rows;
+        private readonly int columns;
+        private Grid grid;
+        private Random random = new Random();
+
+        public Shipwright(int rows, int columns)
+        {
+            this.columns = columns;
+            this.rows = rows;
+        }
+
+        public Fleet CreateFleet(IEnumerable<int> shipLengths)
+        {
+
+            Fleet fleet = null;
+            for (int i = 0; i < 3; ++i)
+            {
+
+                List<int> lengths = new List<int>(shipLengths.OrderByDescending(l => l));
+                grid = new Grid(rows, columns);
+                fleet = new Fleet();
+
+                while (lengths.Count > 0)
+                {
+
+                    var placements = grid.GetAvailablePlacements(lengths[0]);
+
+                    if (placements.Count() == 0)
+                        break;
+
+                    lengths.Remove(0);
+                    int index = random.Next(0, placements.Count());
+                    fleet.AddShip(placements.ElementAt(index));
+                    // 6. Eliminate squares from grid
+
+                    if (lengths.Count == 0)
+                        return fleet;
+                }
+            }
+            throw new ArgumentOutOfRangeException();
+        }
     }
 }
