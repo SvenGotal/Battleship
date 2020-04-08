943253119bf4d95c4f711948abd5ef40af4efd2c
Auro Mazzi <auro.mazzi@gmail.com>
2020-04-08 20:58:22 +0200

    Add Shipwright class

---
+++ Model/Shipwright.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Shipwright
+    {
+        public Shipwright(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+        }
+        public Fleet CreateFleet(IEnumerable<int> shipLengths)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                List<int> lengths = new List<int>(shipLengths.OrderByDescending(x => x));
+                // 1. create grid
+                grid = new Grid(rows, columns);
+                // 2. create fleet
+                Fleet fleet = new Fleet();
+
+
+                while (lengths.Count > 0)
+                {
+                    // 3. get available positions from grid for given length
+                    var placements = grid.GetAvailablePlacements(lengths[0]);
+                    if (placements.Count() == 0)
+                        break;
+                    lengths.Remove(0);
+                    // 4. select one position
+                    int index = random.Next(0, placements.Count());
+                    // 5. forward position to fleet to create ship
+                    fleet.AddShip(placements.ElementAt(index));
+                    // 6. eliminate squares from grid
+
+                    if (lengths.Count == 0)
+                        return fleet;
+                }
+                
+            }
+            throw new ArgumentOutOfRangeException();
+
+        }
+
+        private readonly int rows;
+        private readonly int columns;
+        private Grid grid;
+        private Random random = new Random();
+    }
+}
