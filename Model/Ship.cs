a6fece600d915105976fc92d6470d717e6ca0cfc
Ante Mišković <amiskovic0101@gmail.com>
2020-04-08 20:17:38 +0200

    Added ship, fleet and override Equals for Square

---
+++ Model/Ship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Ship
+    {
+        public Ship ( IEnumerable <Square> Squares)
+        {
+            this.Squares = Squares; 
+        }
+        public readonly IEnumerable<Square> Squares;
+    }
+}
