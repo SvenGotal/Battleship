9cba044230b24fd2da121e2a34d1b89513c3a523
Paško Berišić <paberisi@vsite.hr>
2020-04-08 20:17:36 +0200

    Add Ship, Fleet and override Equals of Square

---
+++ UnitTests/TestShip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestShip
+    {
+        [TestMethod]
+        public void ConstructorCreatesShipFromSquaresProvided()
+        {
+            Ship ship = new Ship(new List<Square> { new Square(1, 4), new Square(1, 5), new Square(1, 6) });
+            Assert.IsTrue(ship.Squares.Contains(new Square(1, 4)));
+            Assert.IsTrue(ship.Squares.Contains(new Square(1, 5)));
+            Assert.IsTrue(ship.Squares.Contains(new Square(1, 6)));
+        }
+    }
+}
