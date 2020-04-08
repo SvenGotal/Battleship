3fd62ac66b7502b8034942196d566c465cea45bd
Fran Safranec <Fran@DESKTOP-QN7J9VS>
2020-04-08 20:25:09 +0200

    add test

---
+++ UnitTests/UnitTest2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    [TestClass]
+    public class TestFleet
+    {
+        [TestMethod]
+        public void AddShipCrreatesNewShipInTheFleet()
+        {
+            Fleet fleet = new Fleet();
+            Assert.IsTrue(ship.Squares.Contains(new Square(1, 4)));
+            Assert.IsTrue(ship.Squares.Contains(new Square(1, 5)));
+            Assert.IsTrue(ship.Squares.Contains(new Square(1, 6)));
+        }
+    }
+}
