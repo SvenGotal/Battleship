06fbdad0f0550afa55f75a8b8c94a9c820dc8578
Darijb <darij.b@gmail.com>
2020-04-08 20:17:51 +0200

    Add Ship, Fleet and override Equals

---
+++ Model/Ship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Ship
+    {
+        public Ship(IEnumerable<Square> squares) {
+            this.squares = squares;
+        }
+        public readonly IEnumerable<Square> squares;
+
+    }
+}
