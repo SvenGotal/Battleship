965b7b5e7bf44ffb1e66d5ae6fd9d7f606e1634d
Ivan Mihin <ivmihin@vsite.hr>
2020-04-08 21:59:44 +0200

    Fix Fleet class and TestFleet

--- Model/Fleet.cs
+++ Model/Fleet.cs
@@ -6,18 +6,19 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
-    public class Fleet
+    public class Fleet  
     {
         public void AddShip(IEnumerable<Square> squares)
         {
             ships.Add(new Ship(squares));
         }
-    }
 
-    public IEnumerable<Ship> Ships
-    {
-        get { return ships; }
-    }
 
-    private List<Ship> ships = new List<Ship>();
+        public IEnumerable<Ship> Ships
+        {
+            get { return ships; }
+        }
+
+        private List<Ship> ships = new List<Ship>();
+    }
 }
