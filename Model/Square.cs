0edf26337ed84f0d36a5dfb590a074d25ac3d7ab
Robert Bakota <robakota@vsite.hr>
2020-04-08 20:19:25 +0200

    Add Fleet

--- Model/Square.cs
+++ Model/Square.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
-    public class Square
+    public class Square : IEquatable<Square>
     {
         public Square(int row, int column)
         {
@@ -14,6 +14,23 @@ namespace Vsite.Oom.Battleship.Model
             Column = column;
         }
         public readonly int Row;
-        public readonly int Column; 
+        public readonly int Column;
+
+        public bool Equals(Square other)
+        {
+            return Row == other.Row && Column == other.Column;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (obj.GetType() != GetType())
+                return false;
+            return Equals((Square)obj);
+        }
+        public override int GetHashCode()
+        {
+            return Row ^ Column;
+        }
     }
 }
