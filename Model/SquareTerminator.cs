aa0370c1b5ee005c00ca0e09f2eb164e8bde0877
Fran Safranec <Fran@DESKTOP-QN7J9VS>
2020-04-08 21:42:38 +0200

    add sqaureTerminator

---
+++ Model/SquareTerminator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class SquareTerminator
+    {
+        public SquareTerminator(Grid grid)
+        {
+            this.grid = grid;
+        }
+
+        public IEnumerable<Square> toEliminate(IEnumerable<Square> shipSquares)
+        {
+
+         
+
+            throw new NotImplementedException();
+        }
+
+        private readonly Grid grid;
+    }
+}
---
+++ UnitTests/UnitTest3.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    [TestClass]
+    public class TestSquareTerminator
+    {
+        [TestMethod]
+        public void ToEliminateReturns18SquaresAroundTheShip4_6to4_6()
+        {
+            Grid grid = new Grid(10, 10);
+            SquareTerminator terminator = new SquareTerminator(grid);
+           var toEliminate =  terminator.toEliminate(new List<Square> { new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6) });
+
+            Assert.AreEqual(18, toEliminate.Count());
+            Assert.IsTrue(toEliminate.Contains(new Square(3, 2)));
+            Assert.IsTrue(toEliminate.Contains(new Square(5, 2)));
+            Assert.IsTrue(toEliminate.Contains(new Square(3, 7)));
+            Assert.IsTrue(toEliminate.Contains(new Square(5, 7)));
+        }
+
+        public void ToEliminateReturns18SquaresAroundTheShip4_6to4_6()
+        {
+            Grid grid = new Grid(10, 10);
+            SquareTerminator terminator = new SquareTerminator(grid);
+            var toEliminate = terminator.toEliminate(new List<Square> { new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6) });
+
+            Assert.AreEqual(18, toEliminate.Count());
+            Assert.IsTrue(toEliminate.Contains(new Square(3, 2)));
+            Assert.IsTrue(toEliminate.Contains(new Square(5, 2)));
+            Assert.IsTrue(toEliminate.Contains(new Square(3, 7)));
+            Assert.IsTrue(toEliminate.Contains(new Square(5, 7)));
+        }
+
+
+        public void ToEliminateReturns18SquaresAroundTheShip4_6to4_6()
+        {
+            Grid grid = new Grid(10, 10);
+            SquareTerminator terminator = new SquareTerminator(grid);
+            var toEliminate = terminator.toEliminate(new List<Square> { new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6) });
+
+            Assert.AreEqual(18, toEliminate.Count());
+            Assert.IsTrue(toEliminate.Contains(new Square(3, 2)));
+            Assert.IsTrue(toEliminate.Contains(new Square(5, 2)));
+            Assert.IsTrue(toEliminate.Contains(new Square(3, 7)));
+            Assert.IsTrue(toEliminate.Contains(new Square(5, 7)));
+        }
+
+
+        public void ToEliminateReturns18SquaresAroundTheShip4_6to4_6()
+        {
+            Grid grid = new Grid(10, 10);
+            SquareTerminator terminator = new SquareTerminator(grid);
+            var toEliminate = terminator.toEliminate(new List<Square> { new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6) });
+
+            Assert.AreEqual(18, toEliminate.Count());
+            Assert.IsTrue(toEliminate.Contains(new Square(3, 2)));
+            Assert.IsTrue(toEliminate.Contains(new Square(5, 2)));
+            Assert.IsTrue(toEliminate.Contains(new Square(3, 7)));
+            Assert.IsTrue(toEliminate.Contains(new Square(5, 7)));
+        }
+
+        public void ToEliminateReturns18SquaresAroundTheShip4_6to4_6()
+        {
+            Grid grid = new Grid(10, 10);
+            SquareTerminator terminator = new SquareTerminator(grid);
+            var toEliminate = terminator.toEliminate(new List<Square> { new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6) });
+
+            Assert.AreEqual(18, toEliminate.Count());
+            Assert.IsTrue(toEliminate.Contains(new Square(3, 2)));
+            Assert.IsTrue(toEliminate.Contains(new Square(5, 2)));
+            Assert.IsTrue(toEliminate.Contains(new Square(3, 7)));
+            Assert.IsTrue(toEliminate.Contains(new Square(5, 7)));
+        }
+    }
+}
