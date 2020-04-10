b2a7dd9bfde9f520327580fff3652d4f20f82557
Ivan Zoric <ivzoric@vsite.hr>
2020-04-10 20:59:41 +0200

    Comment out debugging snippets of code

--- BattleshipGUI/Form1.cs
+++ BattleshipGUI/Form1.cs
@@ -23,7 +23,7 @@ namespace BattleshipGUI
         private void Grid_Paint(object sender, PaintEventArgs e)
         {
             while (CreateGrid() == 404) {
-                gr = new modelNmspc.Grid(10, 10);
+                gr = new modelNmspc.Grid(10, 10);                             //petlja se vrti dok createGrid uspješno ne izgenerira valjanu flotu
                 fl = new modelNmspc.fleet();
                 Console.WriteLine("!fail!");
             }
@@ -51,28 +51,28 @@ namespace BattleshipGUI
             }
             return false;
         }
-        private void PrintOutFleetPositions()
-        {
-            var shipsList = fl.Ships;
-            using (var sequenceEnum1 = shipsList.GetEnumerator())
-            {
-                while (sequenceEnum1.MoveNext())                                               //debugging purpose only
-                {
-                    var ship = sequenceEnum1.Current;
-                    var squares = ship.squares;
-                    using (var sequenceEnum2 = squares.GetEnumerator())
-                    {
-                        while (sequenceEnum2.MoveNext())
-                        {
-                            var square = sequenceEnum2.Current;
-                            Console.WriteLine("col = " + square.column + " row=" + square.row);
+        //private void PrintOutFleetPositions()
+        //{
+        //    var shipsList = fl.Ships;
+        //    using (var sequenceEnum1 = shipsList.GetEnumerator())
+        //    {
+        //        while (sequenceEnum1.MoveNext())                                               //debugging purpose only
+        //        {
+        //            var ship = sequenceEnum1.Current;
+        //            var squares = ship.squares;
+        //            using (var sequenceEnum2 = squares.GetEnumerator())
+        //            {
+        //                while (sequenceEnum2.MoveNext())
+        //                {
+        //                    var square = sequenceEnum2.Current;
+        //                    Console.WriteLine("col = " + square.column + " row=" + square.row);
 
-                        }
-                        Console.WriteLine("ship over---!");
-                    }
-                }
-            }
-        }
+        //                }
+        //                Console.WriteLine("ship over---!");
+        //            }
+        //        }
+        //    }
+        //}
         private void DrawGrid()
         {  
             Graphics graphobject = grid.CreateGraphics();                      
@@ -83,7 +83,7 @@ namespace BattleshipGUI
             Pen grayPen = new Pen(grayBrush, 8);
             int column = 0;
             int row = 0;
-            PrintOutFleetPositions();                                                //debugging purpose only
+            /*PrintOutFleetPositions(); */                                               //debugging purpose only
             for (int i = 0; i != 500; i += 50)
             {
                 for (int j = 0; j != 500; j += 50)
@@ -110,7 +110,7 @@ namespace BattleshipGUI
             gr = new modelNmspc.Grid(10, 10);
             fl = new modelNmspc.fleet();
             while (CreateGrid() == 404)
-            {
+            {                                                 //petlja se vrti dok createGrid uspješno ne izgenerira valjanu flotu
                 gr = new modelNmspc.Grid(10, 10);
                 fl = new modelNmspc.fleet();
                 Console.WriteLine("!fail!");
