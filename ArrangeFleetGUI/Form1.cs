11b8444225f6e2a4404cd97bf56f0ffea4b048ad
mvuki <mvuki@LAPTOP-7EO7DRN2>
2020-04-11 11:17:07 +0200

    Added ArrangeFleetGUI

---
+++ ArrangeFleetGUI/Form1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Build.BuildEngine;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vsite.Oom.Battleship.Model;
+
+namespace ArrangeFleetGUI
+{
+    public partial class Battleship : Form
+    {
+        public Battleship()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+        }
+
+       
+
+        private void button101_Click(object sender, EventArgs e)
+        {
+            
+
+            String buttonname;
+            List<Button> listofbuttons = this.Controls.OfType<Button>()
+                 .Concat(this.panel1.Controls.OfType<Button>())
+                 .ToList();
+            List<Button> buttonsclicked = new List<Button>();
+            Shipwright shipwright = new Shipwright(10, 10);
+            var fleet = shipwright.CreateFleet(new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 });
+            var flag = 0; 
+           
+            var listofships = fleet.Ships.ToList();
+            var rowlist = new List<int>();
+            var collist = new List<int>();
+
+            foreach (var a in listofships)
+            {
+                var squares = a.Squares;
+                foreach (var b in squares)
+                {
+                    var r = b.Row;
+                    var c = b.Column;
+
+                    for (int i = 0; i < 10; ++i)
+                    {
+                        for (int j = 0; j < 10; j++)
+                        {
+                            if (i == c && r == j)
+                            {
+                                buttonname = "row" + i + "col" + j;
+                                foreach (var but in listofbuttons)
+                                {
+                                    if (but.Name == buttonname)
+                                    {
+                                        but.BackColor = SystemColors.ControlDarkDark;
+                                 
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+                flag = 1;
+            }
+
+
+            if (flag == 1)
+            {
+                button101.Enabled = false;
+               
+                DialogResult dialogResult = MessageBox.Show("Do you want to arrange fleet again?", "No", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                
+                    Battleship form = new Battleship();
+                    form.Show();
+                }
+            
+               
+                
+            }   
+
+        }
+    }
+}
+
--- Model/Shipwright.cs
+++ Model/Shipwright.cs
@@ -7,64 +7,57 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
-  public  class Shipwright
+    public class Shipwright
     {
         public Shipwright(int rows, int columns)
         {
             this.rows = rows;
             this.columns = columns;
-
         }
         public Fleet CreateFleet(IEnumerable<int> shipLengths)
         {
-
-            bool fleetComplete = false;
-            for (int i = 0; i < 3; ++i) { 
-
-            do
+            for (int i = 0; i < 3; ++i)
             {
                 List<int> lengths = new List<int>(shipLengths.OrderByDescending(x => x));
 
+                grid = new Grid(rows, columns); // 1. create grid    
+                SquareTerminator terminator = new SquareTerminator(grid);
+                Fleet fleet = new Fleet(); // 2. create fleet
 
-
-                grid = new Grid(rows, columns);
-
-Fleet  fleet = new Fleet();
-
-                    while (lengths.Count > 0)
-                    {
-                        var placements = grid.GetAvailablePlacements(lengths[0]);
-                        if (placements.Count() == 0)
-                        {
-                            break;
-                        }
-                        lengths.RemoveAt(0);
-
-                        int index = random.Next(0, placements.Count());
-
-                        fleet.AddShip(placements.ElementAt(index));
-                        //6. eliminate squares from grid 
-                        if (lengths.Count == 0)
-                        {
-                            return fleet;
-                        }
-                    }
-                
-                
-            } while (fleetComplete == false);
-}
-            return null;
-           
- }
-           
-
-
-
+                while (lengths.Count > 0)
+                {
+                    var placements = grid.GetAvailablePlacements(lengths[0]); // 3. get available positions from grid for given length
+                    if (placements.Count() == 0)
+                        break;
+                    lengths.RemoveAt(0);
+                    int index = random.Next(0, placements.Count()); // 4. select one position
+                    fleet.AddShip(placements.ElementAt(index)); // 5. forward position to fleet to create ship      
+                    var toEliminate = terminator.ToEliminate(placements.ElementAt(index)); // 6. eliminate squares from grid
+                    grid.EliminateSquares(toEliminate);
+                    if (lengths.Count == 0)
+                        return fleet;
+                }
+            }
+            throw new ArgumentOutOfRangeException();
+        }
        
-
         private readonly int rows;
         private readonly int columns;
-        private  Grid grid;
-        private Random random = new Random();
+        private Grid grid;
+        private Random random = new Random(); 
+        
+        public int getRows()
+        {
+            return this.grid.Rows;
+        }
+        public int getCols()
+        {
+            return this.columns;
+        }
+
+        public Grid getGrid()
+        {
+            return this.grid;
+        }
     }
 }
