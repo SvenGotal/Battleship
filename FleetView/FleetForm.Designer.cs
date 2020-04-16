117406978154ace2c17cba1c58df4cf86d0cfae4
Marko Vitković <mavitkov@vsite.hr>
2020-04-16 20:35:23 +0200

    add button and grid

--- FleetView/FleetForm.Designer.cs
+++ FleetView/FleetForm.Designer.cs
@@ -28,10 +28,26 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "FleetForm";
+			this.buttonAgrees = new System.Windows.Forms.Button();
+			this.gridForFleet = new FleetView.GridForFleet();
+			this.SuspendLayout();
+			//grid for fleet
+			this.gridForFleet.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+			this.gridForFleet.Name = "gridForFleet";
+			this.gridForFleet.Text = "gridForFleet";
+			this.gridForFleet.Location = new System.Drawing.Point(10, 10);
+			this.gridForFleet.Size = new System.Drawing.Size(400, 400);
+			this.gridForFleet.TabIndex = 0;
+			//button
+			this.buttonAgrees.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonAgrees.Name = "buttonAgrees";
+			this.buttonAgrees.Text = "&Arrange";
+			this.buttonAgrees.Location = new System.Drawing.Point(318, 415);
+			this.buttonAgrees.Size = new System.Drawing.Size(80, 25);
+			this.buttonAgrees.TabIndex = 1;
+			this.buttonAgrees.UseVisualStyleBackColor = true;
+			this.buttonAgrees.Click += new System.EventHandler(this.buttonAgreesOnClick);
+			
 		}
 
 		#endregion
