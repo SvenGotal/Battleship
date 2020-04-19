3b97e0699f0db6de0d68335f0ff52a0210444071
Roland Rostohar <rorostoh@vsite.hr>
2020-04-19 22:00:34 +0200

    All done (hardcoded values for now)

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace GUI
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.CreateFleetButton = new System.Windows.Forms.Button();
+			this.GameBoard = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// CreateFleetButton
+			// 
+			this.CreateFleetButton.Location = new System.Drawing.Point(337, 426);
+			this.CreateFleetButton.Name = "CreateFleetButton";
+			this.CreateFleetButton.Size = new System.Drawing.Size(75, 23);
+			this.CreateFleetButton.TabIndex = 0;
+			this.CreateFleetButton.Text = "Slozi";
+			this.CreateFleetButton.UseVisualStyleBackColor = true;
+			this.CreateFleetButton.Click += new System.EventHandler(this.CreateFleetButton_Click);
+			// 
+			// GameBoard
+			// 
+			this.GameBoard.Location = new System.Drawing.Point(0, 10);
+			this.GameBoard.Margin = new System.Windows.Forms.Padding(0);
+			this.GameBoard.Name = "GameBoard";
+			this.GameBoard.Size = new System.Drawing.Size(424, 413);
+			this.GameBoard.TabIndex = 1;
+			this.GameBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawSquares);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(424, 461);
+			this.Controls.Add(this.GameBoard);
+			this.Controls.Add(this.CreateFleetButton);
+			this.MaximumSize = new System.Drawing.Size(440, 500);
+			this.MinimumSize = new System.Drawing.Size(440, 500);
+			this.Name = "Form1";
+			this.Text = "Flota";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button CreateFleetButton;
+		private System.Windows.Forms.Panel GameBoard;
+	}
+}
+
---
+++ GUI/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vsite.Oom.Battleship.Model;
+
+namespace GUI
+{
+	public partial class Form1 : Form
+	{
+		Rectangle[,] allFields;
+		Graphics graphics;
+
+		public Form1()
+		{
+			InitializeComponent();
+			allFields = new Rectangle[10, 10];
+			graphics = GameBoard.CreateGraphics();
+		}
+
+		private void CreateFleetButton_Click(object sender, EventArgs e)
+		{
+			RefreshMap();
+
+			List<int> ships = new List<int>
+			{
+				2,2,2,2,3,3,3,4,4,5
+			};
+			Fleet fleet = new Fleet();
+			Shipwright shipwright = new Shipwright(10, 10);
+			fleet = shipwright.CreateFleet(ships);
+
+			Size fillSize = new Size(39, 39);
+
+			for (int r = 0; r < 10; r++)
+			{
+				for (int c = 0; c < 10; c++)
+				{
+					foreach (var ship in fleet.Ships)
+					{
+						if (ship.Squares.Contains(new Square(r, c)))
+						{
+							Rectangle fillRectangle = allFields[r, c];
+							fillRectangle.Size = fillSize;
+							fillRectangle.X += 1;
+							fillRectangle.Y += 1;
+							graphics.FillRectangle(new SolidBrush(Color.DarkBlue), fillRectangle);
+
+						}
+					}
+				}
+
+			}
+					
+
+		}
+
+		private void DrawSquares(object sender, PaintEventArgs e)
+		{
+				Pen pen = new Pen(Color.DarkGray, 1);
+				Size squareSize = new Size(40, 40);
+				Size fillSize = squareSize;
+				fillSize.Width -= 1;
+				fillSize.Height -= 1;
+
+				for (int r = 0; r < 10; r++)
+				{
+					for (int c = 0; c < 10; c++)
+					{
+						allFields[r, c] = new Rectangle(new Point((r * 40) + 10, (c * 40) + 5), squareSize);
+						graphics.DrawRectangle(pen, allFields[r, c]);
+
+						Rectangle fillRectangle = new Rectangle(new Point((r * 40) + 10 + 1, (c * 40) + 5 + 1), fillSize);
+						graphics.FillRectangle(new SolidBrush(Color.White), fillRectangle);
+					}
+				}
+
+				pen.Dispose();
+		}
+
+		private void RefreshMap()
+		{
+			Size fillSize = new Size(39, 39);
+
+			for (int r = 0; r < 10; r++)
+			{
+				for (int c = 0; c < 10; c++)
+				{
+					Rectangle fillRectangle = allFields[r, c];
+					fillRectangle.Size = fillSize;
+					fillRectangle.X += 1;
+					fillRectangle.Y += 1;
+					graphics.FillRectangle(new SolidBrush(Color.White), fillRectangle);
+				}
+			}
+		}
+	}
+}
