4130e9cbce5a323ef62b0df8808a681c48e4efd6
Domagoj Salkić <dosalkic@vsite.hr>
2020-04-20 21:21:17 +0200

    Add function DrawShips for drawing fleet on grid

--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -22,14 +22,15 @@ namespace Vsite.Oom.Battleship.GUI
 
         private void drawButton_Click(object sender, EventArgs e)
         {
-            DrawFleet(g);
+            g.Clear(Color.LightBlue);
+            DrawFleet();
         }
 
 
-        private void DrawFleet(Graphics g)
+        private void DrawFleet()
         {
             myPen = new Pen(Color.Black, 2);
-            myBrush = new SolidBrush(Color.Blue);
+            myBrush = new SolidBrush(Color.DimGray);
             float x = 0f;
             float y = 0f;
             if (Int32.TryParse(numberOfLinesTextBox.Text, out numOfLines) && (numOfLines > 9 && numOfLines < 21))
@@ -50,6 +51,9 @@ namespace Vsite.Oom.Battleship.GUI
                     g.DrawLine(myPen, x, y, distanceBetweenLines * numOfLines, y);
                     y += distanceBetweenLines;
                 }
+
+                DrawShips();
+
             }
             else
             {
@@ -57,6 +61,19 @@ namespace Vsite.Oom.Battleship.GUI
             }
         }
 
+        public void DrawShips()
+        {
+            Shipwright sw = new Shipwright(numOfLines,numOfLines);
+            var fleet = sw.CreateFleet(new int[] {5, 4, 4, 3, 3, 3, 2, 2, 2, 2});
+
+            foreach (var ship in fleet.Ships)
+            {
+                foreach (var square in ship.Squares)
+                {
+                    g.FillRectangle(myBrush,square.Column * distanceBetweenLines,square.Row * distanceBetweenLines,distanceBetweenLines,distanceBetweenLines);
+                }
+            }
+        }
 
         private Pen myPen;
         private Brush myBrush;
