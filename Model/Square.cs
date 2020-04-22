c6b11c72973751cf8d5fe6f217369e12666bda8a
Paško Berišić <paberisi@vsite.hr>
2020-04-22 21:25:41 +0200

    Add Hittin ships

--- Model/Square.cs
+++ Model/Square.cs
@@ -12,6 +12,7 @@ namespace Vsite.Oom.Battleship.Model
         {
             Row = row;
             Column = column;
+            Hit = false;
         }
         public readonly int Row;
         public readonly int Column;
@@ -29,6 +30,11 @@ namespace Vsite.Oom.Battleship.Model
             if (obj.GetType() != GetType())
                 return false;
             return Equals((Square)obj);
+        }
+
+        public bool Hit
+        {
+            get; set;
         }
 
         public override int GetHashCode()
