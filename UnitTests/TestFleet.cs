c01443b957a132ef589464f94f8d5fb96d957ed8
Mario Bilic <mario.bilic4@gmail.com>
2020-04-22 21:38:14 +0200

    add hit in TestFleet

--- UnitTests/TestFleet.cs
+++ UnitTests/TestFleet.cs
@@ -21,5 +21,37 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             fleet.AddShip(new List<Square> { new Square(4, 5), new Square(5, 5), new Square(6, 5) });
             Assert.AreEqual(2, fleet.Ships.Count());
         }
+        [TestMethod]
+        public void HitShipsReturnsResultOfShooting()
+        {
+            Fleet fleet = new Fleet();
+            fleet.AddShip(new List<Square> { new Square(1, 4), new Square(1, 5), new Square(1, 6) });
+            fleet.AddShip(new List<Square> { new Square(4, 5), new Square(5, 5) });
+
+            var hit = fleet.Hit(new Square(1, 4));
+            Assert.AreEqual(HitResult.Hit, hit);
+
+            hit = fleet.Hit(new Square(1, 5));
+            Assert.AreEqual(HitResult.Hit, hit);
+
+            hit = fleet.Hit(new Square(1, 6));
+            Assert.AreEqual(HitResult.Sunk, hit);
+
+            hit = fleet.Hit(new Square(1, 9));
+            Assert.AreEqual(HitResult.Missed, hit);
+
+            hit = fleet.Hit(new Square(4, 5));
+            Assert.AreEqual(HitResult.Hit, hit);
+
+            hit = fleet.Hit(new Square(5, 5));
+            Assert.AreEqual(HitResult.Sunk, hit);
+
+            Assert.IsTrue(fleet.Ships.First().Squares.Contains(new Square(1, 4)));
+            Assert.IsTrue(fleet.Ships.First().Squares.Contains(new Square(1, 5)));
+            Assert.IsTrue(fleet.Ships.First().Squares.Contains(new Square(1, 6)));
+
+            fleet.AddShip(new List<Square> { new Square(4, 5), new Square(5, 5), new Square(6, 5) });
+            Assert.AreEqual(2, fleet.Ships.Count());
+        }
     }
 }
