6b6fcbc1a9321196d01dba4e0e840072cef2cc23
Borna Brišar Ivković <bobrisar@vsite.hr>
2020-04-22 21:42:02 +0200

    Add Hit method in Fleet

--- UnitTests/TestFleet.cs
+++ UnitTests/TestFleet.cs
@@ -18,10 +18,37 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             Assert.IsTrue(fleet.Ships.First().Squares.Contains(new Square(1, 4)));
             Assert.IsTrue(fleet.Ships.First().Squares.Contains(new Square(1, 5)));
             Assert.IsTrue(fleet.Ships.First().Squares.Contains(new Square(1, 6)));
-                          
+
             fleet.AddShip(new List<Square> { new Square(4, 5), new Square(5, 5), new Square(6, 5) });
             Assert.AreEqual(2, fleet.Ships.Count());
 
         }
+
+        [TestMethod]
+        public void HitShipReturnsResultOfShooting()
+        { 
+            Fleet fleet = new Fleet();
+            fleet.AddShip(new List<Square> { new Square(1, 4), new Square(1, 5), new Square(1, 6) });
+            fleet.AddShip(new List<Square> { new Square(4, 5), new Square(5, 5) });
+
+            var hit = fleet.Hit(new Square(1, 4));
+            Assert.AreEqual(HitResult.Hit, hit);
+
+            hit = fleet.Hit(new Square(1, 5));
+            Assert.AreEqual(HitResult.Hit, hit);
+
+            hit = fleet.Hit(new Square(1, 6));
+            Assert.AreEqual(HitResult.Sunken, hit);
+
+            hit = fleet.Hit(new Square(1, 9));
+            Assert.AreEqual(HitResult.Missed, hit);
+
+            hit = fleet.Hit(new Square(4, 5));
+            Assert.AreEqual(HitResult.Hit, hit);
+
+            hit = fleet.Hit(new Square(5, 5));
+            Assert.AreEqual(HitResult.Sunken, hit);
+        }
+
     }
 }
