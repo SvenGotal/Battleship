1c4f44356c1c285bae19367fc5c6ac50b28bc177
Robert Bakota <robakota@vsite.hr>
2020-04-22 21:25:56 +0200

    Add Hitting ships

--- UnitTests/TestShip.cs
+++ UnitTests/TestShip.cs
@@ -16,5 +16,38 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             Assert.IsTrue(ship.Squares.Contains(new Square(1, 5)));
             Assert.IsTrue(ship.Squares.Contains(new Square(1, 6)));
         }
+        [TestMethod]
+        public void HitMethodOnFirstSquareReturnsHitResultHit()
+        {
+            {
+                Ship ship = new Ship(new List<Square> { new Square(1, 4), new Square(1, 5), new Square(1, 6) });
+                var result = ship.Hit(new Square(1, 5));
+                Assert.AreEqual(HitResult.Hit, result);
+            }
+            {
+                Ship ship = new Ship(new List<Square> { new Square(1, 4), new Square(1, 5), new Square(1, 6) });
+                var result = ship.Hit(new Square(1, 4));
+                Assert.AreEqual(HitResult.Hit, result);
+            }
+            {
+                Ship ship = new Ship(new List<Square> { new Square(1, 4), new Square(1, 5), new Square(1, 6) });
+                var result = ship.Hit(new Square(1, 6));
+                Assert.AreEqual(HitResult.Hit, result);
+            }
+        }
+        public void HitMethodMissedSquareReturnsHitResultMissed()
+        {
+            Ship ship = new Ship(new List<Square> { new Square(1, 4), new Square(1, 5), new Square(1, 6) });
+            var result = ship.Hit(new Square(1, 5));
+            Assert.AreEqual(HitResult.Hit, result);
+        }
+        public void HitMethodMissedSquareReturnsHitResultMissed()
+        {
+            Ship ship = new Ship(new List<Square> { new Square(1, 4), new Square(1, 5), new Square(1, 6) });
+            var result = ship.Hit(new Square(1, 5));
+            result = ship.Hit()
+        }
+
+
     }
 }
