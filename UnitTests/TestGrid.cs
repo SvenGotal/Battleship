3f3aa993853a41da787e7a6f987ec1727abbb8e1
banov.josipa <banov.josipa@gmail.com>
2020-04-22 14:54:34 +0200

    uff

--- UnitTests/TestGrid.cs
+++ UnitTests/TestGrid.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Vsite.Oom.Battleship.Model
+namespace Vsite.Oom.Battleship.Model.UnitTests
 {
     [TestClass]
     public class TestGrid
@@ -18,40 +18,36 @@ namespace Vsite.Oom.Battleship.Model
             Assert.AreEqual(3, result.First().Count());
             Assert.AreEqual(3, result.Last().Count());
         }
-
         [TestMethod]
-        public void GetAvailablePlacementsForShipReturns3PlacementsForShipOfLength3InVerticalGrid5x1()
+        public void GetAvailablePlacementsForShipReturns2PlacementsForShipOfLength3InVerticallGrid5x1()
         {
             Grid g = new Grid(5, 1);
             var result = g.GetAvailablePlacements(3);
             Assert.AreEqual(3, result.Count());
 
-            foreach(var sequence in result)
-            Assert.AreEqual(3, sequence.Count());
+            foreach (var sequence in result)
+                Assert.AreEqual(3, sequence.Count());
         }
 
-            [TestMethod]
-            public void GetAvailablePlacementsForShipReturns3PlacementsForShipOfLength2InHorizontalGrid1x6AfterSquareIsEliminated()
-            {
-                Grid g = new Grid(1, 6);
-                g.EliminateSquares(new List<Square> { new Square(0,2)});
-                var result = g.GetAvailablePlacements(3);
-                Assert.AreEqual(3, result.Count());
+        [TestMethod]
+        public void GetAvailablePlacementsForShipReturns3PlacementsForShipOfLength2InHorizontalGrid1x6AfterSquareIsEliminated()
+        {
+            Grid g = new Grid(1, 6);
+            g.EliminateSquares(new List<Square> { new Square(0, 2) });
+            var result = g.GetAvailablePlacements(2);
+            Assert.AreEqual(3, result.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementsForShipReturns2PlacementsForShipOfLength2InVerticallGrid5x1AfterSquareIsEliminated()
+        {
+            Grid g = new Grid(5, 1);
+            g.EliminateSquares(new List<Square> { new Square(1, 0) });
+            var result = g.GetAvailablePlacements(2);
+            Assert.AreEqual(2, result.Count());
 
             foreach (var sequence in result)
                 Assert.AreEqual(2, sequence.Count());
         }
-
-            [TestMethod]
-            public void GetAvailablePlacementsForShipReturns3PlacementsForShipOfLength2InVerticalGrid5x1AfterSquareIsEliminated()
-            {
-                Grid g = new Grid(5, 1);
-                g.EliminateSquares(new List<Square> { new Square(1, 0) });
-                var result = g.GetAvailablePlacements(3);
-                Assert.AreEqual(2, result.Count());
-
-            foreach (var sequence in result)
-                Assert.AreEqual(2, sequence.Count());
-        }
-     }
+    }
 }
