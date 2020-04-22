b4c89322f300171be3a51153fccc6b26a2644096
mvuki <mvuki@LAPTOP-7EO7DRN2>
2020-04-22 20:22:04 +0200

    Extract fleet creation into separate method

--- Model/Shipwright.cs
+++ Model/Shipwright.cs
@@ -18,29 +18,34 @@ namespace Vsite.Oom.Battleship.Model
         {
             for (int i = 0; i < 3; ++i)
             {
-                List<int> lengths = new List<int>(shipLengths.OrderByDescending(x => x));
 
-                grid = new Grid(rows, columns); // 1. create grid    
-                SquareTerminator terminator = new SquareTerminator(grid);
-                Fleet fleet = new Fleet(); // 2. create fleet
-
-                while (lengths.Count > 0)
-                {
-                    var placements = grid.GetAvailablePlacements(lengths[0]); // 3. get available positions from grid for given length
-                    if (placements.Count() == 0)
-                        break;
-                    lengths.RemoveAt(0);
-                    int index = random.Next(0, placements.Count()); // 4. select one position
-                    fleet.AddShip(placements.ElementAt(index)); // 5. forward position to fleet to create ship      
-                    var toEliminate = terminator.ToEliminate(placements.ElementAt(index)); // 6. eliminate squares from grid
-                    grid.EliminateSquares(toEliminate);
-                    if (lengths.Count == 0)
-                        return fleet;
-                }
+                Fleet fleet = PlaceShips(shipLengths);
+                if (fleet != null)
+                    return fleet;
             }
             throw new ArgumentOutOfRangeException();
         }
-       
+       private Fleet PlaceShips(IEnumerable<int> shipLengths)
+        {
+        List<int> lengths = new List<int>(shipLengths.OrderByDescending(x => x));
+
+        grid = new Grid(rows, columns); // 1. create grid    
+        SquareTerminator terminator = new SquareTerminator(grid);
+        Fleet fleet = new Fleet(); // 2. create fleet
+
+        while (lengths.Count > 0)
+        {
+            var placements = grid.GetAvailablePlacements(lengths[0]); // 3. get available positions from grid for given length
+            if (placements.Count() == 0)
+                return null;
+            lengths.RemoveAt(0);
+            int index = random.Next(0, placements.Count()); // 4. select one position
+            fleet.AddShip(placements.ElementAt(index)); // 5. forward position to fleet to create ship      
+            var toEliminate = terminator.ToEliminate(placements.ElementAt(index)); // 6. eliminate squares from grid
+            grid.EliminateSquares(toEliminate);
+        }
+                return fleet;
+        }
         private readonly int rows;
         private readonly int columns;
         private Grid grid;
