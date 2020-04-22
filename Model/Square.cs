fb99b7135d1e03b5fe022fe616d7e8b32ec7864f
Ivan Mihin <ivmihin@vsite.hr>
2020-04-22 21:50:12 +0200

    Add Hit methods for Ship and Fleet and Tests for them

--- Model/Square.cs
+++ Model/Square.cs
@@ -12,6 +12,7 @@ namespace Vsite.Oom.Battleship.Model
         {
             Row = row;
             Column = column;
+            Hit = false;
         }
 
         public readonly int Row;
@@ -39,6 +40,8 @@ namespace Vsite.Oom.Battleship.Model
             }
             return Equals((Square)obj);
         }
+
+        public bool Hit { get; set; }
 
         public override int GetHashCode()
         {
