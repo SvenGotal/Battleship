21d89e62fa86639c73f1cab3da534b84a5be1dde
Ante Mišković <amiskovic0101@gmail.com>
2020-04-22 20:22:32 +0200

    1.commit

--- Model/Shipwright.cs
+++ Model/Shipwright.cs
@@ -16,37 +16,39 @@ namespace Vsite.Oom.Battleship.Model
         }
         public Fleet CreateFleet(IEnumerable<int> shipLengths)
         {
-            bool fleetComplete = false;
-            
-            do
+            for(int i = 0; i < 3; ++i)
             {
-                List<int> lengths = new List<int>(shipLengths.OrderByDescending(x => x));
-
-                grid = new Grid(rows, columns);
-                SquareTerminator terminator = new SquareTerminator(grid);
-                Fleet fleet = new Fleet();
-
-                
-                    while (lengths.Count > 0)
-                    {
-                        var placements = grid.GetAvailablePlacements(lengths[0]);
-                        lengths.RemoveAt(0);
-                        if (placements.Count() == 0)
-                            break;
-                        int index = random.Next(0, placements.Count());
-                        fleet.AddShip(placements.ElementAt(index));
-                        
-                    var toEliminate=terminator.ToEliminate(placements.ElementAt(index));
-                        grid.EliminateSquares(toEliminate);
-
-                        if (lengths.Count() == 0)
-                            return fleet;
-                    }
-
-            } while (fleetComplete == false);
-            return null;
+                Fleet fleet = PlaceShips(shipLengths);
+                if(fleet!=null)
+                    return fleet;
+            }
+            throw new ArgumentOutOfRangeException();
            
         }
+        private Fleet PlaceShips(IEnumerable<int> shipLengths)
+        {
+
+            List<int> lengths = new List<int>(shipLengths.OrderByDescending(x => x));
+
+            grid = new Grid(rows, columns);
+            SquareTerminator terminator = new SquareTerminator(grid);
+            Fleet fleet = new Fleet();
+
+
+            while (lengths.Count > 0)
+            {
+                var placements = grid.GetAvailablePlacements(lengths[0]);
+                lengths.RemoveAt(0);
+                if (placements.Count() == 0)
+                    return null;
+                int index = random.Next(0, placements.Count());
+                fleet.AddShip(placements.ElementAt(index));
+
+                var toEliminate = terminator.ToEliminate(placements.ElementAt(index));
+                grid.EliminateSquares(toEliminate);
+            }
+            return fleet;
+        }    
         private readonly int rows;
         private readonly int columns;
         private Grid grid;
