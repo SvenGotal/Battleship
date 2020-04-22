6f887b67f9b10b00711b6baa8e0aad209fd5bf6f
banov.josipa <banov.josipa@gmail.com>
2020-04-22 20:34:14 +0200

    Add Equality and ineq for square.

--- Model/Square.cs
+++ Model/Square.cs
@@ -6,14 +6,48 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
-   public class Square
+    public class Square : IEquatable<Square>
     {
         public Square(int row, int column)
         {
             Row = row;
             Column = column;
         }
+
         public readonly int Row;
         public readonly int Column;
+
+        public bool Equals(Square other)
+        {
+            if(other == null)
+            {
+                return false;
+            }
+            return Row == other.Row && Column == other.Column;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj.GetType() != GetType())
+                return false;
+
+            return Equals((Square)obj);
+        }
+        public override int GetHashCode()
+        {
+            return Row ^ Column;
+        }
+
+        public static bool operator==(Square lhs,Square rhs)
+        {
+            return Equals(lhs, rhs);
+        }
+
+        public static bool operator !=(Square lhs, Square rhs)
+        {
+            return !(lhs == rhs);
+        }
     }
 }
